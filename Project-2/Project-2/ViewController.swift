177e94db485c5685860404f04a4d13621891f86a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-04-29 14:30:15 +0200

    [Project-2]: Designing the layout

--- Project-2/Project-2/ViewController.swift
+++ Project-2/Project-2/ViewController.swift
@@ -1,19 +1,14 @@
-//
-//  ViewController.swift
-//  Project-2
-//
-//  Created by Erik Sebastian De Erice on 29/4/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
