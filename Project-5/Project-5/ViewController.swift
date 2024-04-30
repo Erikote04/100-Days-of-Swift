49f2ee0775cd162cfbe316eb7ecdc65228e44802
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-04-30 15:45:25 +0200

    [Project-5]: Or else what?

--- Project-5/Project-5/ViewController.swift
+++ Project-5/Project-5/ViewController.swift
@@ -51,6 +51,8 @@ class ViewController: UITableViewController {
     
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
         
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
@@ -59,9 +61,26 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    errorTitle = "Word don't recognize"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else {
+                errorTitle = "Word already used"
+                errorMessage = "Be more original!"
             }
+        } else {
+            guard let title = title else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title.lowercased())."
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .destructive))
+        
+        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
