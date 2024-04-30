0c3f42587eda76a4467af78ec03353defbb1217c
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-04-30 13:55:51 +0200

    [Project-5]: Reading from disk. contentsOf

--- Project-5/Project-5/ViewController.swift
+++ Project-5/Project-5/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  Project-5
-//
-//  Created by Erik Sebastian De Erice on 30/4/24.
-//
-
 import UIKit
 
 class ViewController: UITableViewController {
 
+    var allWards = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWards = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWards.isEmpty {
+            allWards = ["silkworm"]
+        }
+        
+        startGame()
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
+    }
+    
+    func startGame() {
+        title = allWards.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
 }
 
