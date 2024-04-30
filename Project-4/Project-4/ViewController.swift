ae3e4e8a4a51c9b88f9e2a44adf3edb8deee23bf
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-04-30 09:50:50 +0200

    [Project-4]: Setting up

---
+++ Project-4/Project-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-4
+//
+//  Created by Erik Sebastian De Erice on 30/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
