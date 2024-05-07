773bc10de61e0e4a378ebddcb3692f6ba6ad2994
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-07 13:28:44 +0200

    [Project-6B]: Auto Layout metrics and priorities. Constraints (withVisualFormat)

--- Project-6B/Project-6B/ViewController.swift
+++ Project-6B/Project-6B/ViewController.swift
@@ -41,11 +41,11 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1":label1,
-                               "label2":label2,
-                               "label3":label3,
-                               "label4":label4,
-                               "label5":label5]
+        let viewsDictionary = ["label1": label1,
+                               "label2": label2,
+                               "label3": label3,
+                               "label4": label4,
+                               "label5": label5]
         
         for label in viewsDictionary.keys {
             view.addConstraints(NSLayoutConstraint.constraints(
@@ -56,10 +56,12 @@ class ViewController: UIViewController {
             )
         }
         
+        let metrics = ["labelHeight": 88]
+        
         view.addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]",
+            withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|",
             options: [],
-            metrics: nil,
+            metrics: metrics,
             views: viewsDictionary)
         )
     }
