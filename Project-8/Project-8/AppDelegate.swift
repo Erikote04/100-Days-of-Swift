a2cb2c70b007e877885fc64b524f4f14574ce152
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-08 12:33:30 +0200

    [Project-8]: Building a UIKit user interface programmatically

--- Project-8/Project-8/AppDelegate.swift
+++ Project-8/Project-8/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  Project-8
-//
-//  Created by Erik Sebastian De Erice on 8/5/24.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
