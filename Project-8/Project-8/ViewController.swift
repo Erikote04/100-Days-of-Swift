0c5d7bc56aa21859dac7b8711cdd84b2c083552f
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-08 11:35:54 +0200

    [Project-8]: Setting up

---
+++ Project-8/Project-8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-8
+//
+//  Created by Erik Sebastian De Erice on 8/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
