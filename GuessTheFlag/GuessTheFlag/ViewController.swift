baca0bff41c65705dfea3f52f1d1a88e04c08bb7
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-01-07 14:25:08 +0100

    [Project-2]: Making the basic game work. UIButton and CALayer

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -8,13 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries = [
+            "estonia",
+            "france",
+            "germany",
+            "ireland",
+            "italy",
+            "monaco",
+            "nigeria",
+            "poland",
+            "spain",
+            "uk",
+            "us"
+        ]
+        
+        setupView()
+        askQuestion()
+    }
+    
+    func setupView() {
+        NSLayoutConstraint.activate([
+            button1.widthAnchor.constraint(equalToConstant: 200),
+            button2.widthAnchor.constraint(equalToConstant: 200),
+            button3.widthAnchor.constraint(equalToConstant: 200),
+            button1.heightAnchor.constraint(equalToConstant: 100),
+            button2.heightAnchor.constraint(equalToConstant: 100),
+            button3.heightAnchor.constraint(equalToConstant: 100),
+        ])
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 }
